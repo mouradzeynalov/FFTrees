74d746db341eb6b67ea82ea02be0d4b424bdf616
Mourad Zeynalov <mourad@mouradzeynalov.org>
2025-05-01 13:13:39 -0700

    debug

--- R/fftrees_grow_fan.R
+++ R/fftrees_grow_fan.R
@@ -313,8 +313,8 @@ fftrees_grow_fan <- function(x,
                  # Factor, rank by num thresholds
                  cat("The index is : ", i, "\n")
                  print(cue_best_df_current$threshold[[i]])
-                 split <- strsplit(as.character(cue_best_df_current$threshold[[i]], ",", perl = TRUE, useBytes = FALSE, fixed = TRUE))[[1]]
-                 ifelse(!is.na(split), length(split), Inf)
+                 x = strsplit(as.character(cue_best_df_current$threshold[[i]], ",", perl = TRUE, useBytes = FALSE, fixed = TRUE))[[1]]
+                 ifelse(!is.na(x), length(x), Inf)
              } else {
                  # Numeric, set threshold to infinity
                  print("Numeric cue, setting to infinity")

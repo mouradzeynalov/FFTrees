d3f3175891424d5aa58a60e5abf3fae64a56d0f3
Mourad Zeynalov <mourad@mouradzeynalov.org>
2025-08-06 10:40:42 -0700

    debug

--- R/util_gfft.R
+++ R/util_gfft.R
@@ -104,7 +104,6 @@ read_fft_df <- function(ffts_df, tree = 1){
   # Get 1 line by tree ID (ffts_df may be unsorted):
   cur_fft <- ffts_df[(ffts_df$tree == tree), ]
   # print(cur_fft)  # 4debugging
-  print(tree)       # 4debugging
 
   # Key values:
   # fft_node_sep <- ";"  # (local constant)
@@ -230,6 +229,7 @@ write_fft_df <- function(fft, tree = -99L){
   # Key values:
   # fft_node_sep <- ";"  # (local constant)
   nodes_n <- nrow(fft)
+  cat("nrow for tree: ", tree, " is ", nodes_n, "\n")
 
 
   # Main: ----

cd8478842b2665513df717a93755d4c15df7673d
Mourad Zeynalov <mourad@mouradzeynalov.org>
2025-12-19 14:19:55 -0500

    Removing Redundant Return Values

--- R/FFTrees.R
+++ R/FFTrees.R
@@ -475,6 +475,11 @@ FFTrees <- function(formula = NULL,
                           digits = 2)
 
 
+  # Clear out unecessary return values
+  x$data            <- NULL
+  x$competition     <- NULL
+  x$trees$decisions <- NULL
+
 
   # Provide user feedback: ----
 
